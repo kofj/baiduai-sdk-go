4f05632b12bb16ce49aafcba9bc74d85c3557582
Frank Kung <kfanjian@gmail.com>
2018-09-26 21:55:56 +0800

    使用 url.Values 存储请求参数

--- vision/ocr/param.go
+++ vision/ocr/param.go
@@ -1,7 +1,11 @@
 package ocr
 
+import (
+	"net/url"
+)
+
 // RequestParam OCR 请求参数
-type RequestParam func(map[string]string)
+type RequestParam func(*url.Values)
 
 // LangType 识别语言类型，默认为CHN_ENG。可选值包括：
 // - CHN_ENG：中英文混合；
@@ -39,8 +43,8 @@ func LangType(lang string) RequestParam {
 	if illegal {
 		lang = "CHN_ENG"
 	}
-	return func(m map[string]string) {
-		m["language_type"] = lang
+	return func(m *url.Values) {
+		m.Set("language_type", lang)
 	}
 }
 
@@ -48,23 +52,23 @@ func LangType(lang string) RequestParam {
 // - true：检测朝向；
 // - false：不检测朝向。
 func DetectDirection() RequestParam {
-	return func(m map[string]string) {
-		m["detect_direction"] = "true"
+	return func(m *url.Values) {
+		m.Set("detect_direction", "true")
 	}
 }
 
 // DetectLanguage 是否检测语言，默认不检测。
 // 当前支持（中文、英语、日语、韩语）
 func DetectLanguage() RequestParam {
-	return func(m map[string]string) {
-		m["detect_language"] = "true"
+	return func(m *url.Values) {
+		m.Set("detect_language", "true")
 	}
 }
 
 // WithProbability 是否返回识别结果中每一行的置信度
 func WithProbability() RequestParam {
-	return func(m map[string]string) {
-		m["probability"] = "true"
+	return func(m *url.Values) {
+		m.Set("probability", "true")
 	}
 }
 
@@ -72,37 +76,37 @@ func WithProbability() RequestParam {
 // - big：不定位单字符位置，默认值；
 // - small：定位单字符位置
 func RecognizeGranularity() RequestParam {
-	return func(m map[string]string) {
-		m["recognize_granularity"] = "small"
+	return func(m *url.Values) {
+		m.Set("recognize_granularity", "small")
 	}
 }
 
 // WithVertexesLocation 是否返回文字外接多边形顶点位置，不支持单字位置。默认为false
 func WithVertexesLocation() RequestParam {
-	return func(m map[string]string) {
-		m["vertexes_location"] = "true"
+	return func(m *url.Values) {
+		m.Set("vertexes_location", "true")
 	}
 }
 
 // IDCardSideFront 身份证含照片的一面
 func IDCardSideFront() RequestParam {
-	return func(m map[string]string) {
-		m["id_card_side"] = "front"
+	return func(m *url.Values) {
+		m.Set("id_card_side", "front")
 	}
 }
 
 // IDCardSideBack 身份证带国徽的一面
 func IDCardSideBack() RequestParam {
-	return func(m map[string]string) {
-		m["id_card_side"] = "back"
+	return func(m *url.Values) {
+		m.Set("id_card_side", "back")
 	}
 }
 
 // DetectRisk 是否开启身份证风险类型(身份证复印件、临时身份证、身份证翻拍、修改过的身份证)功能，默认不开启，即：false。
 // true-开启；false-不开启
 func DetectRisk() RequestParam {
-	return func(m map[string]string) {
-		m["detect_risk"] = "true"
+	return func(m *url.Values) {
+		m.Set("back", "true")
 	}
 }
 
@@ -111,7 +115,7 @@ func Accuracy(opt string) RequestParam {
 	if opt != "normal" && opt != "high" {
 		opt = "normal"
 	}
-	return func(m map[string]string) {
-		m["accuracy"] = "normal"
+	return func(m *url.Values) {
+		m.Set("back", "normal")
 	}
 }
