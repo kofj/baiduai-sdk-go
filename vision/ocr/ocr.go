9b48b991b375baed2ea08f804877d2aba5e6bb83
Frank Kung <kfanjian@gmail.com>
2018-09-26 22:25:45 +0800

    重构接口代码

--- vision/ocr/ocr.go
+++ vision/ocr/ocr.go
@@ -86,61 +86,96 @@ func (c *Client) doRequest(api string, img *vision.Image, r interface{}, params 
 }
 
 // GeneralBasic ...
-func (c *Client) GeneralBasic(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrGeneralBasicURL, img, params...)
+func (c *Client) GeneralBasic(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrGeneralBasicURL, img, resp, params...)
+	return
 }
 
 // General ...
-func (c *Client) General(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrGeneralURL, img, params...)
+func (c *Client) General(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrGeneralURL, img, resp, params...)
+
+	return
 }
 
 // Accurate ...
-func (c *Client) Accurate(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrAccurateURL, img, params...)
+func (c *Client) Accurate(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrAccurateURL, img, resp, params...)
+
+	return
 }
 
 // GeneralEnhanced ...
-func (c *Client) GeneralEnhanced(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrGeneralEnhancedURL, img, params...)
+func (c *Client) GeneralEnhanced(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrGeneralEnhancedURL, img, resp, params...)
+
+	return
 }
 
 // Webimage ...
-func (c *Client) Webimage(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrWebimageURL, img, params...)
+func (c *Client) Webimage(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrWebimageURL, img, resp, params...)
+
+	return
 }
 
 // Handwriting ...
-func (c *Client) Handwriting(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrHandwritingURL, img, params...)
+func (c *Client) Handwriting(img *vision.Image, params ...RequestParam) (resp *GeneralResp, err error) {
+	resp = &GeneralResp{}
+	err = c.doRequest(OcrHandwritingURL, img, resp, params...)
+
+	return
 }
 
 // Idcard ...
-func (c *Client) Idcard(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrIdcardURL, img, params...)
+func (c *Client) Idcard(img *vision.Image, params ...RequestParam) (resp *IdcardResp, err error) {
+	resp = &IdcardResp{}
+	err = c.doRequest(OcrIdcardURL, img, resp, params...)
+
+	return
 }
 
 // Bankcard ...
-func (c *Client) Bankcard(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrBankcardURL, img, params...)
+func (c *Client) Bankcard(img *vision.Image, params ...RequestParam) (resp *BankcardResp, err error) {
+	resp = &BankcardResp{}
+	err = c.doRequest(OcrBankcardURL, img, resp, params...)
+
+	return
 }
 
 // DrivingLicense ...
-func (c *Client) DrivingLicense(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrDrivingLicenseURL, img, params...)
+func (c *Client) DrivingLicense(img *vision.Image, params ...RequestParam) (resp *DrivingLicenseResp, err error) {
+	resp = &DrivingLicenseResp{}
+	err = c.doRequest(OcrDrivingLicenseURL, img, resp, params...)
+
+	return
 }
 
 // VehiclLicense ...
-func (c *Client) VehiclLicense(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrVehiclLicenseURL, img, params...)
+func (c *Client) VehiclLicense(img *vision.Image, params ...RequestParam) (resp *VehiclLicenseResp, err error) {
+	resp = &VehiclLicenseResp{}
+	err = c.doRequest(OcrVehiclLicenseURL, img, resp, params...)
+
+	return
 }
 
 // LicensePlate ...
-func (c *Client) LicensePlate(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrLicensePlateURL, img, params...)
+func (c *Client) LicensePlate(img *vision.Image, params ...RequestParam) (resp *LicensePlateResp, err error) {
+	resp = &LicensePlateResp{}
+	err = c.doRequest(OcrLicensePlateURL, img, resp, params...)
+
+	return
 }
 
 // BusinessLicense ...
-func (c *Client) BusinessLicense(img *vision.Image, params ...RequestParam) (resp *Resp, err error) {
-	return c.doRequest(OcrBusinessLicenseURL, img, params...)
+func (c *Client) BusinessLicense(img *vision.Image, params ...RequestParam) (resp *BusinessLicenseResp, err error) {
+	resp = &BusinessLicenseResp{}
+	err = c.doRequest(OcrBusinessLicenseURL, img, resp, params...)
+
+	return
 }
